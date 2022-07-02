eb9c9855f3f8dd83e586ee323bb6baf7d2b727d7
Jared Gutkin <jgutkinsam@gmail.com>
2022-07-01 19:07:30 -0500

    register user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -23,6 +23,29 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new Error('user already exists')
     }
 
+    //hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    //create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if(user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+
+
 
     res.json({message: 'register user'})
 })
