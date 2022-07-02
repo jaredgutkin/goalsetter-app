307ae8092644e29f767ec587b4a58db4ba653d61
Jared Gutkin <jgutkinsam@gmail.com>
2022-07-02 07:16:20 -0500

    full crud functionality with authentication

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,12 +1,13 @@
 const asyncHandler = require('express-async-handler')
 
 const Goal = require('../models/goalModel')
+const User = require('../models/userModel')
 
 //@desc Get Goals
 //@route GET /api/goals
 //@acess Private
 const getGoals = asyncHandler(async (req, res) => {
-    const goals = await Goal.find()
+    const goals = await Goal.find({ user: req.user.id})
 
     res.status(200).json(goals)
 })
@@ -21,7 +22,8 @@ const setGoal = asyncHandler(async (req, res) => {
     }
 
     const goal = await Goal.create({
-        text: req.body.text
+        text: req.body.text,
+        user: req.user.id
     })
 
     res.status(200).json(goal)
@@ -37,6 +39,21 @@ const updateGoal = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Goal not found')
     }
+
+    const user = await User.findById(req.user.id)
+
+    //check user
+    if(!user) {
+        res.status(401)
+        throw new Error('user nto found')
+    }
+
+    //make sure logged in user matches goal user
+    if(goal.user.toString() !== user.id){
+        res.status(401)
+        throw new Error('user nto authorized')
+    }
+
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
     res.status(200).json(updatedGoal)
 })
@@ -51,6 +68,21 @@ const deleteGoal = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Goal not found')
     }
+
+    const user = await User.findById(req.user.id)
+
+    //check user
+    if(!user) {
+        res.status(401)
+        throw new Error('user nto found')
+    }
+
+    //make sure logged in user matches goal user
+    if(goal.user.toString() !== user.id){
+        res.status(401)
+        throw new Error('user nto authorized')
+    }
+    
     await goal.remove()
 
     res.status(200).json({id: req.params.id})
