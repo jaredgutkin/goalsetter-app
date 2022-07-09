cc39c8cd330c30b8677a50f80a3926e257ff2c49
Jared Gutkin <jgutkinsam@gmail.com>
2022-07-09 12:19:28 -0500

    goal service and slice updated

--- frontend/src/components/GoalForm.jsx
+++ frontend/src/components/GoalForm.jsx
@@ -1,10 +1,17 @@
 import {useState} from 'react'
-import {useSelector, useDispatch} from 'react-redux'
+import { useDispatch } from 'react-redux'
+import { createGoal } from '../features'
 
 function GoalForm() {
     const [text, setText] = useState('')
-    const onSubmit = e => {
+
+    const dispatch = useDispatch()
+
+    const onSubmit = (e) => {
         e.preventDefault()
+
+        dispatch(createGoal({text}))
+        setText('')
     }
 
   return (
@@ -12,8 +19,13 @@ function GoalForm() {
         <form onSubmit={onSubmit}>
             <div className="form-group">
                 <label htmlFor="text">Goal</label>
-                <input type="text" name='text' id='text' value={text} 
-                onChange={(e) => setText(e.target.value)} />
+                <input 
+                    type="text" 
+                    name='text' 
+                    id='text' 
+                    value={text} 
+                    onChange={(e) => setText(e.target.value)} 
+                />
             </div>
             <div className="form-group">
                 <button className="btn btn-block" type='submit'>Add Goal</button>
