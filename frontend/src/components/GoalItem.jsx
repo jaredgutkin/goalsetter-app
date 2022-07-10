67834f7545127f8a62328a29fa89a855ec6eaeca
Jared Gutkin <jgutkinsam@gmail.com>
2022-07-10 16:17:46 -0500

    delete goal added

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -1,13 +1,20 @@
 import { FaSkull } from 'react-icons/fa'
+import {useDispatch} from 'react-redux'
+import {deleteGoal} from '../features/goals/goalSlice'
 
 function GoalItem({goal}) {
+
+    const dispatch = useDispatch()
   return (
     <div className="goal">
         <div>
             {new Date(goal.createdAt).toLocaleString('en-US')}
         </div>
         <h2>{goal.text}</h2>
-        <button className="close"><FaSkull/></button>
+        <button onClick={() => dispatch(deleteGoal(goal._id))}
+        className="close">
+            <FaSkull/>
+        </button>
     </div>
   )
 }
